4761ad34147d1e6d591ce5f522bf5a9a0846f58c
federicosimone <federico.simone15@gmail.com>
2025-10-27 22:06:25 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// inserire i età e km utente 
+
+const distUt = prompt("Inserisci i Km che vuoi percorrere")
+const etaUt = prompt ("Inserisci la tua età")
+
+//
+
+console.log(distUt)
+
+
+
+
+
+
