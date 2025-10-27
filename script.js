dff5cb7721323345d35458a16be2378cc3cc6fed
federicosimone <federico.simone15@gmail.com>
2025-10-27 22:44:05 +0100

    risultato

--- script.js
+++ script.js
@@ -4,11 +4,28 @@ const distUt = prompt("Inserisci i Km che vuoi percorrere");
 const etaUt = prompt("Inserisci la tua età");
 const distanza = parseInt(distUt);
 const eta = parseInt(etaUt);
+let risultato = null;
+let price = distanza * 0.21
+
 
 //
 
 console.log(distanza);
 console.log(eta);
+console.log(price)
+
+//svolgimento
+
+if (eta < 18) {
+    risultato = price - (price / 100) * 20 ;
+} else if (eta >= 18 && eta < 65) {
+    risultato = price; 
+} else if (eta >= 65) {
+    risultato = price - (price / 100) * 60 ;
+}
+
+
+alert(risultato);
 
 
 
