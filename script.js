570d69edea37ebd3c2ca26ddd9dfdaf9511a800f
federicosimone <federico.simone15@gmail.com>
2025-10-27 22:23:49 +0100

    variabili

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 // inserire i età e km utente 
 
-const distUt = prompt("Inserisci i Km che vuoi percorrere")
-const etaUt = prompt ("Inserisci la tua età")
+const distUt = prompt("Inserisci i Km che vuoi percorrere");
+const etaUt = prompt("Inserisci la tua età");
+const distanza = parseInt(distUt);
+const eta = parseInt(etaUt);
 
 //
 
-console.log(distUt)
+console.log(distanza);
+console.log(eta);
 
 
 
